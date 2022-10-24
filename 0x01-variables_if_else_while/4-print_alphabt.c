a05c99b83fc59b60e90c33885ff0fe98c697db7c
3fifiQ <mohammed.himmat@gmail.com>
2022-10-24 14:22:13 -0700

    small alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Alawys 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
